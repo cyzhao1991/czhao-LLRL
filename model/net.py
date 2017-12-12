526758d3624b00570870403ff8f184b1b8f59757
ChenyangZhao <Doudou@ChenyangZhaos-MacBook-Pro.local>
2017-12-12 17:33:15 +0000

    trpo commit

--- model/net.py
+++ model/net.py
@@ -11,39 +11,125 @@ class Net(object):
 		raise NotImplementedError
 
 
-class Fcnn_2side(Net):
+class Fcnn(Net):
 
-	def __init__(self, sess, input_dim1, input_dim2, output_dim, layer_dim_1, layer_dim_2, layer_dim_o = [], name = None, **kwargs):
-		super(Fcnn_2side, self).__init__(sess)
-		self.input_dim1 = input_dim1
-		self.input_dim2 = input_dim2
+	def __init__(self, sess, input_dim, output_dim, layer_dim, name = None, **kwargs):
+		super(Fcnn, self).__init__(sess)
+		self.input_dim = input_dim
 		self.output_dim = output_dim
-		self.layer_dim_1 = layer_dim_1
-		self.layer_dim_2 = layer_dim_2
-		self.layer_dim_o = layer_dim_o
+		self.layer_dim = layer_dim
 		self.name = name
 
-		self.merge_method = kwargs['merge_method'] if 'merge_method' in kwargs.keys() else 'add'
-		self.if_bias = kwargs['if_bias'] if 'if_bias' in kwargs.keys() else True
+		self.all_layer_dim = np.concatenate([[self.input_dim], layer_dim, [self.output_dim]], axis = 0)
+		self.if_bias = kwargs.get('if_bias', ([True] * len(layer_dim))+[False] )
+		self.activation_fns = kwargs.get( 'activation', (['tanh']*len(layer_dim))+[None])
+		with tf.name_scope(self.name):
+			self.input = kwargs.get('input_tf', tf.placeholder(tf.float32, [None, self.input_dim], name = 'input'))
 
-		self.all_layer_dim_1 = np.concatenate([[self.input_dim1], layer_dim_1], axis = 0)
-		self.all_layer_dim_2 = np.concatenate([[self.input_dim2], layer_dim_2], axis = 0)
-		if self.merge_method is 'concatenate':
-			self.all_layer_dim_o = np.concatenate([[self.layer_dim_1[-1] + self.layer_dim_2[-1]], layer_dim_o], axis = 0)
-		else:
-			assert(self.layer_dim_1[-1] == self.layer_dim_2[-1])
-			self.all_layer_dim_o = np.concatenate([[self.layer_dim_1[-1]], layer_dim_o], axis = 0)
+		assert(self.input.get_shape()[-1].value == input_dim)
+		assert(len(self.activation_fns) == len(layer_dim) + 1)
+		assert(len(self.if_bias) == len(layer_dim) + 1)
 
-		self.input_1, self.input_2, self.output = self.build(name = self.name)
+		self.output = self.build(self.input, self.name)
 
-	def build(self, name):
-
+	def build(self, input_tf, name):
+		
 		with tf.name_scope(name):
-			self.input_1 = tf.placeholder(tf.float32, [None, self.input_dim1], name = 'input1')
-			self.input_2 = tf.placeholder(tf.float32, [None, self.input_dim2], name = 'input2')
-			net_1 = []
-			net_2 = []
-			for i, dim in enumerate(self.all_layer_dim_1):
-				
+			net = [input_tf]
+			weights = []
+			if np.any(self.if_bias):
+				bias = []
 
+			for i, (dim_1, dim_2) in enumerate( zip(self.all_layer_dim[:-1], self.all_layer_dim[1:]) ):
+				if self.if_bias[i]:
+					weights.append( tf.Variable( tf.truncated_normal([dim_1, dim_2], stddev = 1.0), name = 'weights_%i'%i))
+					bias.append( tf.Variable (tf.truncated_normal([dim_2], stddev = 1.0), name = 'bias_%i'%i))
+					if self.activation_fns[i] is 'tanh':
+						net.append( tf.nn.tanh( tf.matmul(net[i], weights[i]) + bias[-1]))
+					else if self.activation_fns[i] is 'relu':
+						net.append( tf.nn.relu( tf.matmul(net[i], weights[i]) + bias[-1]))
+					else if self.activation_fns[i] is None:
+						net.append( tf.matmul(net[i], weights[i]) + bias[-1] )
+				else:
+					weights.append( tf.Variable( tf.truncated_normal([dim_1, dim_2], stddev = 1.0), name = 'weights_%i'%i))
+					bias.append( tf.Variable (tf.truncated_normal([dim_2], stddev = 1.0), name = 'bias_%i'%i))
+					if self.activation_fns[i] is 'tanh':
+						net.append( tf.nn.tanh( tf.matmul(net[i], weights[i])))
+					else if self.activation_fns[i] is 'relu':
+						net.append( tf.nn.relu( tf.matmul(net[i], weights[i])))
+					else if self.activation_fns[i] is None:
+						net.append( tf.matmul(net[i], weights[i]))
 
+			return net[-1]
+
+# class Fcnn_2side(Net):
+
+# 	def __init__(self, sess, input_dim1, input_dim2, output_dim, layer_dim_1, layer_dim_2, layer_dim_o = [], name = None, **kwargs):
+# 		super(Fcnn_2side, self).__init__(sess)
+# 		self.input_dim1 = input_dim1
+# 		self.input_dim2 = input_dim2
+# 		self.output_dim = output_dim
+# 		self.layer_dim_1 = layer_dim_1
+# 		self.layer_dim_2 = layer_dim_2
+# 		self.layer_dim_o = layer_dim_o
+# 		self.name = name
+
+# 		self.merge_method = kwargs.get('merge_method', 'add')
+# 		self.if_bias = kwargs.get('if_bias', True)
+# 		# self.merge_method = kwargs['merge_method'] if 'merge_method' in kwargs.keys() else 'add'
+# 		# self.if_bias = kwargs['if_bias'] if 'if_bias' in kwargs.keys() else True
+# 		self.all_layer_dim_1 = np.concatenate([[self.input_dim1], layer_dim_1], axis = 0)
+# 		self.all_layer_dim_2 = np.concatenate([[self.input_dim2], layer_dim_2], axis = 0)
+# 		if self.merge_method is 'concatenate':
+# 			self.all_layer_dim_o = np.concatenate([[self.layer_dim_1[-1] + self.layer_dim_2[-1]], layer_dim_o], axis = 0)
+# 		else if self.merge_method in ['add', 'multiply']:
+# 			assert(self.layer_dim_1[-1] == self.layer_dim_2[-1])
+# 			self.all_layer_dim_o = np.concatenate([[self.layer_dim_1[-1]], layer_dim_o], axis = 0)
+
+# 		self.input_1, self.input_2, self.output = self.build(name = self.name)
+
+# 	def build(self, name):
+
+# 		with tf.name_scope(name):
+# 			self.input_1 = tf.placeholder(tf.float32, [None, self.input_dim1], name = 'input1')
+# 			self.input_2 = tf.placeholder(tf.float32, [None, self.input_dim2], name = 'input2')
+# 			net_1 = [self.input_1]
+# 			net_2 = [self.input_2]
+# 			weights_1 = []
+# 			weights_2 = []
+# 			if self.if_bias: 
+# 				bias_1 = []
+# 				bias_2 = []
+
+# 			for i, (dim_1, dim_2) in enumerate( zip(self.all_layer_dim_1[:-1], self.all_layer_dim_1[1:]) ):
+# 				if self.if_bias:
+# 					weights_1.append(tf.Variable( tf.truncated_normal([dim_1, dim_2], stddev = 1.0), name = 'weights_1_layer_%i'%(i) ))
+# 					bias_1.append( tf.Variable( tf.truncated_normal([dim_2], stddev = 1.), name = 'bias_1_layer_%i'%(i) ))
+# 					net_1.append( tf.nn.tanh( tf.matmul(net_1[i], weights_1[i]) + bias_1[i]) )
+# 				else:
+# 					weights_1.append(tf.Variable( tf.truncated_normal([dim_1, dim_2], stddev = 1.0), name = 'weights_1_layer_%i'%(i) ))
+# 					net_1.append( tf.nn.tanh( tf.matmul(net_1[i], weights_1[i]) ) )
+
+# 			for i, (dim_1, dim_2) in enumerate( zip(self.all_layer_dim_2[:-1], self.all_layer_dim_2[1:]) ):
+# 				if self.if_bias:
+# 					weights_2.append( tf.Variable( tf.truncated_normal([dim_1, dim_2], stddev = 1.0), name = 'weights_2_layer_%i'%(i) ))
+# 					bias_2.append( tf.Variable (tf.truncated_normal([dim_2], stddev = 1.), name = 'bias_2_layer_%i'%(i) ))
+# 					net_2.append( tf.nn.tanh( tf.matmul(net_2[i], weights_2[i]) + bias_2[i]) )
+# 				else:
+# 					weights_2.append( tf.Variable( tf.truncated_normal([dim_1, dim_2], stddev = 1.0), name = 'weights_2_layer_%i'%(i) ))
+# 					net_2.append( tf.nn.tanh( tf.matmul(net_2[i], weights_2[i]) ) )
+
+# 			net_o = []
+# 			weights_o = []
+# 			if self.if_bias:
+# 				bias_o = []
+# 			if self.merge_method is 'concatenate':
+# 				net_o.append( tf.concat([net_1[-1],net_2[-1]], axis = 1) )
+# 			else if self.merge_method is 'add':
+# 				net_o.append( net_1[-1] + net_2[-1] )
+# 			else if self.merge_method is 'multiply':
+# 				net_o.append( net_1[-1] * net_2[-1] )
+
+# 			for i, (dim_1, dim_2) in enumerate( zip(self.all_layer_dim_o[:-1], self.all_layer_dim_o[1:]) ):
+# 				if self.if_bias:
+# 					weights_o.append()
