f22f6f81e2ce4dea63e3681e6dfb05ce458b3ad2
cyzhao1991 <cyzhao1991@gmail.com>
2017-12-20 14:15:40 +0000

    delete files

--- main_stl.bash
+++
@@ -1,5 +0,0 @@
-for ((i=0; i<100; i++))
-do
-	python stl.py --exp_num $((i))
-	# echo "experiment $((i)) done." >> log.txt
-done
--- test.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-import model.knowledge_base as kb
-
-L = kb.KnowledgeBase([300,300], 50)
-s = kb.PathPolicy(10,5, L)
