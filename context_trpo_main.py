15d9d54e9ecf3631f11b8fdd05d9878f3f27f55f
cyzhao1991 <cyzhao1991@gmail.com>
2018-02-05 18:06:44 +0000

    context trpo

--- context_trpo_main.py
+++ context_trpo_main.py
@@ -21,8 +21,8 @@ def main(gpu_num, exp_num, env = None, **kwargs):
 	num_of_tasks = kwargs.get('num_of_tasks', 10)
 	# task_num = kwarg.get('task_num', 0)
 	# num_of_paths = kwarg.get('num_of_paths', 100)
-	dir_name = 'Data/checkpoint/'
-	# dir_name = '/disk/scratch/chenyang/Data/context_trpo/mod_%i_exp%i/'%(mod_num, exp_num)
+	# dir_name = 'Data/checkpoint/'
+	dir_name = '/disk/scratch/chenyang/Data/context_trpo/mod_%i_exp%i/'%(mod_num, exp_num)
 	if not os.path.isdir(dir_name):
 		os.makedirs(dir_name)
 
