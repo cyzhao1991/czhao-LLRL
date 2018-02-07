3e0ab7320f80d8dcac6e8b789e99618759616845
cyzhao1991 <cyzhao1991@gmail.com>
2018-02-07 14:15:01 +0000

    context trpo

--- model/context_net.py
+++ model/context_net.py
@@ -37,7 +37,7 @@ class Context_Fcnn_Net(Net):
 
 	def def_Task_knowledge(self, name):
 		with tf.name_scope(name):
-			self.c_weights = [tf.Variable(tf.truncated_normal([self.context_dim, dim], mean = 1.0, stddev = .3), \
+			self.c_weights = [tf.Variable(tf.truncated_normal([self.context_dim, dim], mean = .1, stddev = .03), \
 				name = 's_vector_h%i'%k) for dim,k in zip(self.module_num, range(self.num_of_layer))]
 			self.s_vector = [ tf.expand_dims(tf.matmul(self.context, tf.nn.relu(w_c)), 1) for w_c in self.c_weights]
 			# self.s_vector = [ tf.expand_dims(tf.matmul(self.context, w_c), 1) for w_c in self.c_weights]
